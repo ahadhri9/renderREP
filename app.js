a2d61f39b1a8b5137bcdda22b0f7a6e021cefce1
ahadhri9 <achrafhadhri9@gmail.com>
2022-09-13 09:59:30 +0200

    lal

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => res.type('html').send(html));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
 app.all("/*", (req, res) => {
