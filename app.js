fa84589fd5ace77f7f5c8757050e8184e68d8952
ahadhri9 <achrafhadhri9@gmail.com>
2023-02-14 10:23:59 +0100

    refreshtoken

--- app.js
+++ app.js
@@ -6,41 +6,33 @@ const request = require('request')
 
 const port = process.env.PORT || 3001;
 app.use(express.json())
-  //refreshKeys : regarde s'il existe deja un refreshToken, si oui, génére une nouvelle paire de token, sinon exit.
-  function refreshKeys(){
-    rawdata=fs.readFileSync('token.json')
-    jsonToken=JSON.parse(rawdata);
-    refreshToken = jsonToken.refreshToken
-    if(!refreshToken){
-      console.error(" no refresh token ")
-      return
+
+const clientId = live1_25713_n8xQ0kTslpOmSW4Zyt7dbj1P;
+const clientSecret = r5JKaS7Tc6kOy9q2xIDEHpYjWvuXdBVl;
+//refreshKeys : regarde s'il existe deja un refreshToken, si oui, génére une nouvelle paire de token, sinon exit.
+const getNewRefreshToken = async (clientId, clientSecret) => {
+  const response = await fetch(`https://api.cloudbeds.com/oauth/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=refresh_token`);
+  const data = await response.json();
+  return data;
+  
+}
+
+const checkRefreshToken = async (clientId, clientSecret) => {
+  try {
+    const tokenData = fs.readFileSync('token.json');
+    const token = JSON.parse(tokenData);
+    if (!token.refresh_token) {
+      const newTokens = await getNewRefreshToken(clientId, clientSecret);
+      token.refresh_token = newTokens.refresh_token;
+      token.access_token = newTokens.access_token;
+      fs.writeFileSync('token.json', JSON.stringify(newTokens));
     }
-  var options = {
-    'method': 'POST',
-    'url': 'https://hotels.cloudbeds.com/api/v1.1/access_token',
-    'headers': {
-      'Cookie': 'acessa_session=cf6c3aa21587859cce8fc8f6fc3031e5c9c32f64; HotelLng=en',
-      'host':'hotels.cloudbeds.com'
-    },
-    formData: {
-      'grant_type': 'refresh_token',
-      'client_id': 'live1_25713_n8xQ0kTslpOmSW4Zyt7dbj1P',
-      'client_secret': 'r5JKaS7Tc6kOy9q2xIDEHpYjWvuXdBVl',
-      'refresh_token': refreshToken
-    }
-  };
-  request(options, function (error, response) {
-    if (error) throw new Error(error);
-    json = JSON.parse(response.body)
-    accessToken = json.access_token;//TODO asynchronic task CRITICAL/!\
-    refreshToken = json.refresh_token;
-    console.log(refreshToken)
-    gotoJson = {refreshToken:refreshToken}
-    fs.writeFileSync('./token.json',JSON.stringify(gotoJson))
-  });
+  } catch (error) {
+    console.error(error);
   }
-  refreshKeys()
-  setInterval(refreshKeys,3500000);
+}
+checkRefreshToken(clientId, clientSecret)
+setInterval(()=>checkRefreshToken(clientId, clientSecret),3500000);
 app.all("/*", (req, res) => {
   const msg = {
     protocol: req.protocol,
