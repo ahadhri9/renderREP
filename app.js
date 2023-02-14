82a2c95ae38fd4301b9bbed21e77ec07e0fb7bc5
ahadhri9 <achrafhadhri9@gmail.com>
2023-02-14 09:39:46 +0100

    refreshtoken

--- app.js
+++ app.js
@@ -2,6 +2,41 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(express.json())
+  //refreshKeys : regarde s'il existe deja un refreshToken, si oui, génére une nouvelle paire de token, sinon exit.
+  function refreshKeys(){
+    rawdata=fs.readFileSync('token.json')
+    jsonToken=JSON.parse(rawdata);
+    refreshToken = jsonToken.refreshToken
+    if(!refreshToken){
+      console.error(" no refresh token ")
+      return
+    }
+  var options = {
+    'method': 'POST',
+    'url': 'https://hotels.cloudbeds.com/api/v1.1/access_token',
+    'headers': {
+      'Cookie': 'acessa_session=cf6c3aa21587859cce8fc8f6fc3031e5c9c32f64; HotelLng=en',
+      'host':'hotels.cloudbeds.com'
+    },
+    formData: {
+      'grant_type': 'refresh_token',
+      'client_id': 'live1_25713_n8xQ0kTslpOmSW4Zyt7dbj1P',
+      'client_secret': 'r5JKaS7Tc6kOy9q2xIDEHpYjWvuXdBVl',
+      'refresh_token': refreshToken
+    }
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    json = JSON.parse(response.body)
+    accessToken = json.access_token;//TODO asynchronic task CRITICAL/!\
+    refreshToken = json.refresh_token;
+    console.log(refreshToken)
+    gotoJson = {refreshToken:refreshToken}
+    fs.writeFileSync('./token.json',JSON.stringify(gotoJson))
+  });
+  }
+  refreshKeys()
+  setInterval(refreshKeys,3500000);
 app.all("/*", (req, res) => {
   const msg = {
     protocol: req.protocol,
@@ -19,6 +54,7 @@ app.all("/*", (req, res) => {
     const ReservationID = msg.body.reservationID
     console.log("ReservationID"+ReservationID)
   });
+
 
 
 
