6cb2e06f2d798164514cea5e6e0dc9def41de664
ahadhri9 <achrafhadhri9@gmail.com>
2023-02-16 14:52:40 +0100

    getReservationID_OK

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const { response } = require("express");
 const express = require("express");
 const app = express();
-const fs = require('fs');
-import ('node-fetch')
+const fs = require("fs");
+import("node-fetch");
 var accessToken;
 var refreshToken;
 
 const port = process.env.PORT || 3001;
-app.use(express.json())
+app.use(express.json());
 async function getNewAccessToken() {
-  const tokenData=fs.readFileSync('token.json').toString();
+  const tokenData = fs.readFileSync("token.json").toString();
   const token = JSON.parse(tokenData);
   accessToken = token.access_token;
   refreshToken = token.refresh_token;
 
   var myHeaders = new Headers();
-  myHeaders.append("Cookie", "acessa_session=a5de10117e01531cc0fb1c73c6308150080aa6ef; acessa_session_enabled=1; HotelLng=en");
+  myHeaders.append(
+    "Cookie",
+    "acessa_session=a5de10117e01531cc0fb1c73c6308150080aa6ef; acessa_session_enabled=1; HotelLng=en"
+  );
 
   var formdata = new FormData();
   formdata.append("grant_type", "refresh_token");
@@ -24,31 +27,30 @@ async function getNewAccessToken() {
   formdata.append("refresh_token", refreshToken);
 
   var requestOptions = {
-    method: 'POST',
+    method: "POST",
     headers: myHeaders,
     body: formdata,
-    redirect: 'follow'
+    redirect: "follow",
   };
 
-  resulut = await fetch("https://hotels.cloudbeds.com/api/v1.1/access_token", requestOptions)
-    .then(response => response.text())
-    .catch(error => console.log('error', error));
-    const resultat1 = JSON.parse(resulut);
-    accessToken=resultat1.access_token
-   
-    
+  resulut = await fetch(
+    "https://hotels.cloudbeds.com/api/v1.1/access_token",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .catch((error) => console.log("error", error));
+  const resultat1 = JSON.parse(resulut);
+  accessToken = resultat1.access_token;
+
   if (resulut) {
-    fs.writeFileSync('token.json', resulut);   
+    fs.writeFileSync("token.json", resulut);
     //console.log("resulut:  "+resulut)
-    console.log('New access token generated');
-    
+    console.log("New access token generated");
   }
-  
 }
 // Call this function periodically to check if the access token has expired and generate a new one if needed
-getNewAccessToken()
+getNewAccessToken();
 setInterval(getNewAccessToken, 1000 * 60 * 31); // Check every 30 minutes
-
 
 app.all("/*", async (req, res) => {
   const msg = {
@@ -61,37 +63,38 @@ app.all("/*", async (req, res) => {
     query: req.query,
     params: req.params,
     body: req.body,
+  };
+  res.send(msg);
+  console.log(msg);
+  const ReservationID = msg.body.reservationID;
+  console.log("ReservationID" + ReservationID);
+  //function to get the reservationID
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer " + accessToken);
+  myHeaders.append(
+    "Cookie",
+    "acessa_session=a5de10117e01531cc0fb1c73c6308150080aa6ef; acessa_session_enabled=1; csrf_accessa_cookie=0ac0bba0bffb21019b3ee6bfc978303c; HotelLng=en"
+  );
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  const reservation = await fetch(
+    "https://hotels.cloudbeds.com/api/v1.1/getReservation?reservationID=" +
+      ReservationID,
+    requestOptions
+  )
+    .then((response) => response.text())
+    .catch((error) => console.log("error", error));
+  if (reservation) {
+    console.log("Reservation: " + reservation);
+
+    const guest = Object.values(reservation.data.guestList)[0];
+    console.log("guest:   " + guest);
   }
-    res.send(msg);
-    console.log(msg)
-    const ReservationID = msg.body.reservationID
-    console.log("ReservationID"+ReservationID)
-    //function to get the reservationID
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer "+accessToken);
-    myHeaders.append("Cookie", "acessa_session=a5de10117e01531cc0fb1c73c6308150080aa6ef; acessa_session_enabled=1; csrf_accessa_cookie=0ac0bba0bffb21019b3ee6bfc978303c; HotelLng=en");
-
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
-
-    ResID= await fetch("https://hotels.cloudbeds.com/api/v1.1/getReservation?reservationID="+ReservationID, requestOptions)
-      .then(response => response.text())
-      .catch(error => console.log('error', error));
-      if (ResID) { 
-        console.log("Reservation ID de la mort" + ResID);
-      }
-    
-  });
-
-
-
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-
-
-  
-  
