c45b6b7f1eb152416c260c09a151c17a2c07b1d1
ahadhri9 <achrafhadhri9@gmail.com>
2023-02-20 10:51:38 +0100

    hotelName

--- app.js
+++ app.js
@@ -107,39 +107,14 @@ app.all("/*", async (req, res) => {
         guest_language: guest_language,
         guest_title: guestgender,
       };
-      // var myHeaders = new Headers();
-      // myHeaders.append("Authorization", "Bearer " + accessToken);
-      // myHeaders.append(
-      //   "Cookie",
-      //   "acessa_session=a5de10117e01531cc0fb1c73c6308150080aa6ef; acessa_session_enabled=1; csrf_accessa_cookie=aa03c1f2b14d99081cb6cab7518a5e80; HotelLng=en"
-      // );
-
-      // var requestOptions = {
-      //   method: "GET",
-      //   headers: myHeaders,
-      //   redirect: "follow",
-      // };
-
-      // const hotel = fetch(
-      //   "https://hotels.cloudbeds.com/api/v1.1//getHotels?propertyID=" +
-      //     hotel_id,
-      //   requestOptions
-      // )
-      //   .then((result) => console.log(result))
-      //   .catch((error) => console.log("error", error));
-      // const hotelName = JSON.parse(hotel);
-      // const Hotell = hotelName.data.propertyName;
-      // console.log(Hotell);
-
-      // console.log(hotel);
-      // switch (guestInfo.status) {
-      //   case "checked_out":
-      //     console.log(guestInfo);
-      //     break;
-      //   case "checkin":
-      //     console.log(guestInfo)
-      // }
       console.log(guestInfo);
+      switch (guestInfo.status) {
+        case "checked_out":
+          console.log(guestInfo);
+          break;
+        case "checkin":
+          console.log(guestInfo);
+      }
     }
   }
 });
